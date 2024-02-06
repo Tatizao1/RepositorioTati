9d01c731fd39bc6f9e108a48907d512863c46986
Tatizao1 <laboratorioalvadent627@gmail.com>
2024-02-06 19:04:09 -0300

    no se que poner aca

--- app.js
+++ app.js
@@ -1,4 +1,14 @@
 let express = require('express')
 let app = express();
+const path = require('path')
 
-console.log(app);
+const publicPath = path.resolve(__dirname, './public');
+app.use(express.static(publicPath));
+
+app.listen(3000, () => console.log('Esto fue exitoso'));
+
+app.get('/', (req, res) => {
+    res.sendFile(path.resolve(__dirname, '/views/home.html') )
+})
+
+
